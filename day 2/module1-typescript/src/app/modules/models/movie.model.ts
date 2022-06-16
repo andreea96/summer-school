1cb3ec5a9b75e1fe0d872d43ae1ee2583f15a85f
Robert Constantinescu <robert.constantinescu@bearingpoint.com>
2022-06-16 16:08:18 +0300

    feat: module1-typescript done; chore: updated README

---
+++ day 2/module1-typescript/src/app/modules/models/movie.model.ts
@@ -0,0 +1,11 @@
+import { Actor } from './actor.model';
+
+export class Movie {
+	id: number;
+	name: string;
+	releasedDate: string;
+	director: string;
+	rating: number;
+	duration: number;
+	actors: Actor[];
+}
