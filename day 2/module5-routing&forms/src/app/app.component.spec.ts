1eacbdcbe631719dff1aff957360556b61ba2ebc
radu.deleanu <radu.deleanu@bearingpoint.com>
2022-07-02 21:24:45 +0300

    updated module titles

--- day 2/module5-routing&forms/src/app/app.component.spec.ts
+++ day 2/module5-routing&forms/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('module0-cli app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('module5-routing&forms app is running!');
   });
 });
