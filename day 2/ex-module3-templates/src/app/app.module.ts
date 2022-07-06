b58c5e356475b0b44a88c49ab703328a59ea054d
radu.deleanu <radu.deleanu@bearingpoint.com>
2022-07-06 14:01:34 +0300

    remove pipe imports

--- day 2/ex-module3-templates/src/app/app.module.ts
+++ day 2/ex-module3-templates/src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
-import { MyTimePipe } from './pipes/mytime.pipe';
 import { MovieCardComponent } from './components/movie-card/movie-card.component';
 
 @NgModule({
-	declarations: [AppComponent, MovieListComponent, MovieDetailsComponent, MyTimePipe, MovieCardComponent],
+	declarations: [AppComponent, MovieListComponent, MovieDetailsComponent, MovieCardComponent],
 	imports: [BrowserModule],
 	providers: [],
 	bootstrap: [AppComponent],
